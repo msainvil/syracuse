01e0e0bb87fab8582cfd5a1440d9043b42060c07
msainvil <sainvil95@gmail.com>
2022-02-18 15:28:59 +0100

    syracuse 3a esiea

---
+++ syracuse.py
@@ -0,0 +1,15 @@
+def syracuse(u):
+    iter =0
+    while(u!=1):
+        iter +=1
+        if u%2==0: 
+            u = u//2
+            
+        else:
+            u = 3*u+1
+        print(u)
+    print(iter," : iterations")    
+    return u
+
+number =input()
+syracuse(number)
