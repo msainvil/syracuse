bf03cad3b3e84246721ca00e561418a83a074076
msainvil <sainvil95@gmail.com>
2022-03-04 10:42:56 +0100

    syracuse poo

---
+++ Syracuse_poo.py
@@ -0,0 +1,24 @@
+class Syracuse:
+    def __init__(self):
+        pass
+        with open("./number.txt", "r") as file:
+            number =file.read()
+        self.u = int(number )
+
+    def syracuse_(self):
+        iter =0
+        while(self.u!=1):
+            iter +=1
+            if self.u%2==0: 
+                self.u = self.u//2
+                
+            else:
+               self. u = 3*self.u+1
+            print(self.u)
+        print(iter," : iterations")    
+        return self.u
+
+
+x = Syracuse()
+x.syracuse_()
+
